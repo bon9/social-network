ce4cd9a5b4b88e572b0cd9f25e514a82afa1e481
Oleh <boshaa3@gmail.com>
2019-09-30 20:22:50 +0300

    base template

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://p7.hiclipart.com/preview/968/321/102/telegram-computer-icons-apple-icon-image-format-telegram-icon-enkel-iconset-froyoshark.jpg"
+        alt=""
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <a href="">Profile</a>
+        </li>
+        <li>
+          <a href="">Messages</a>
+        </li>
+        <li>
+          <a href="">MainContent</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
