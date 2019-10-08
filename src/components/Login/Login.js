5b16792e9e1e1db4db6d219108739d43003b285f
Oleh <boshaa3@gmail.com>
2019-10-08 23:37:31 +0300

    add form with final-form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,73 @@
 import React from "react";
+import { Form, Field } from "react-final-form";
+
+const required = value => (value ? undefined : "Required");
+
+const LoginForm = ({ onSubmit }) => {
+  return (
+    <Form
+      onSubmit={onSubmit}
+      initialValues={{ rememberMe: false }}
+      render={({ handleSubmit, form, values, submitting, pristine }) => {
+        return (
+          <form onSubmit={handleSubmit}>
+            <div>
+              <Field
+                type="text"
+                placeholder="Login"
+                name="login"
+                component="input"
+                validate={required}
+              />
+            </div>
+            <div>
+              <Field
+                type="text"
+                placeholder="Password"
+                name="password"
+                component="input"
+              />
+            </div>
+            <div>
+              <Field
+                type="checkbox"
+                name="rememberMe"
+                placeholder="Login"
+                component={"input"}
+              />
+              Remember me
+            </div>
+            <div>
+              <button type="submit" disabled={submitting || pristine}>
+                Login
+              </button>
+              <button
+                type="button"
+                disabled={submitting || pristine}
+                onClick={form.reset}
+              >
+                Reset
+              </button>
+            </div>
+            <pre>{JSON.stringify(values, 0, 2)}</pre>
+          </form>
+        );
+      }}
+    />
+  );
+};
 
 const Login = () => {
-  return <h1>L-L-LOGIN</h1>;
+  const onSubmit = values => {
+    console.log(values);
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <LoginForm onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default Login;
